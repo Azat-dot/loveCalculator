5cb97986cc96905f2f1c3115962b3fac6e0eaefe
azat <azatsafaryan@mail.ru>
2022-06-05 14:11:03 +0500

    second commit

---
+++ script.js
@@ -0,0 +1,8 @@
+prompt("What is your name");
+prompt("what is the name of your favored person")
+
+let n = Math.random();
+let m = Math.floor(n*100) + 1;
+console.log(m);
+
+alert(`Your chances are ${m}  percent`)
