3cd858460454991624801c2c0ab6245b339f9194
azat <azatsafaryan@mail.ru>
2022-06-06 11:44:40 +0500

    leapCalculator second

--- script.js
+++ script.js
@@ -33,11 +33,18 @@
 
 function isLeap(year) {
             
-          if (year%4 === 0 && year%100 !== 0) {
-             return ('Leap year.');
-          }  else if (year%4 === 0 && year%100 === 0 && year%400 === 0){return ('Leap year.');}
-          else {return ('Not leap year.');}
-               
+        //   if (year%4 === 0 && year%100 !== 0) {
+        //      return ('Leap year.');
+        //   }  else if (year%4 === 0 && year%100 === 0 && year%400 === 0){return ('Leap year.');}
+        //   else {return ('Not leap year.');}
+
+        if (year % 4 === 0) {
+            if (year % 100 === 0){
+                if(year % 400 === 0) {
+                    return 'Leap year.'
+                } else {return 'Not leap year.'}
+            } else {return 'Leap year.'}
+        } else {return 'Not leap year.'}         
     }
- alert (isLeap(2002));
+ console.log(isLeap(1990));
 
